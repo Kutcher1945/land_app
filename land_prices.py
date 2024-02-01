51690185fd60a7984136e5ad9148b1ac269c6027
Адилан Ахрамович <a.akhramovich@almatydc.kz>
2024-02-01 14:06:41 +0600

    added a new branch

--- land_prices.py
+++ land_prices.py
@@ -4,27 +4,11 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 # Create a DataFrame with the parameters and land prices
-data = pd.DataFrame({
-    'distance_1000m_ddo': [1, 0, 1, 1, 0],  # Example values
-    'distance_1000m_schools': [1, 0, 0, 1, 1],  # Example values
-    'distance_1000m_medical': [0, 1, 1, 0, 1],  # Example values
-    'deficit_ddo': [10, 5, 8, 12, 7],  # Example values
-    'deficit_schools': [15, 8, 10, 14, 9],  # Example values
-    'amount_dosug_1000m': [3, 2, 4, 5, 1],  # Example values
-    'is_parking_exists': [1, 0, 1, 0, 1],  # Example values
-    'distance_park_1000m': [1, 0, 1, 0, 0],  # Example values
-    'amount_of_cameras_1000m': [4, 3, 2, 5, 1],  # Example values
-    'distance_bikeroad_1000m': [1, 1, 0, 0, 1],  # Example values
-    'amount_of_bins_1000m': [2, 1, 3, 2, 0],  # Example values
-    'amount_of_poi_1000m': [8, 6, 10, 7, 9],  # Example values
-    'amount_of_business19_1000m': [12, 9, 15, 11, 14],  # Example values
-    'index_of_nearest_sensor_pm': [20, 25, 18, 22, 23],  # Example values
-    'index_of_nearest_sensor_am': [30, 28, 35, 32, 31],  # Example values
-    'price': [150000, 160000, 145000, 155000, 158000]  # Example land prices
-})
+data = pd.read_csv('data/train_data.csv')
+data.deficit_schools = data.deficit_schools/100
 
 # Split the dataset into input features (X_train) and target (y_train)
-X_train = data.drop(columns=['price']).values
+X_train = data.drop(columns=['id','price']).values
 y_train = data['price'].values
 
 # Train the model
@@ -40,10 +24,14 @@ st.set_page_config(
 # Create the Streamlit app interface
 st.title("Прогнозирование стоимости земельных участков")
 
+# Add an image after the title
+st.image("pic1.jpeg", use_column_width=True)
+
 # Description
 st.markdown("""
 ##### Веб-приложение для прогнозирования стоимости земельных участков в Алматы.
 
+            
 Это приложение использует машинное обучение для прогнозирования стоимости земельных участков. 
 Оно загружает предварительно обученную модель линейной регрессии, которая принимает на вход различные характеристики участка, 
 такие как расстояние до ДДО, школы, медучреждения, дефицит ДДО и школ, количество объектов досуга, наличие парковки, парка, камер видеонаблюдения, 
@@ -52,38 +40,114 @@ st.markdown("""
 Приложение предварительно обрабатывает входные данные, объединяя некоторые характеристики и добавляя новые, например, расстояние до ближайшего города.
 """)
 
+
+# # Input fields for property characteristics
+# st.header("Характеристики земельного участка")
+# # List of property characteristics
+# property_characteristics = [
+#     "Участок, прилегающий к Роще Баума, в квадрате улиц Сейфуллина, Акан Серы, Успенского, Стоимость, тенге/м 73 000",
+#     "Участок на квадрате улиц Кассина, Акан Сери, Сейфуллина, Хетагурова, Стоимость, тенге/м 75 000",
+#     "Участок в квадрате улиц Сейфуллина, Котельникова, Акан Серы, Стоимость, тенге/м 70 000",
+#     "Участок в квадрате улиц Ауэзова, Жандосова, Манаса, М.Озтюрка, Стоимость, тенге/м 232 000",
+#     "Участок в квадрате улиц Манаса, Бухар жырау, Ауэзова, Габдуллина, Стоимость, тенге/м 231 000",
+#     "Участок на пересечении улиц Бенберина - Тополиная и Бенберина - Шугыла в мкр.Айгерим-1, Стоимость, тенге/м 56 000",
+#     "Участок на пересечении улиц Кисловодская-Левского и 2-ая Кисловодская-Отрарская, Стоимость, тенге/м 72 000",
+#     "Участок в квадрате улиц Абая, Тургут Озала, Брусиловского, Толе би, Стоимость, тенге/м 82 000",
+#     "Участок в квадрате улиц Розыбакиева, Толе би, И.Каримова, Карасай батыра, Стоимость, тенге/м 90 000",
+#     "Участок в мкр. Мамыр - западнее улицы Яссауи, Стоимость, тенге/м 108 000"
+# ]
+# # Create a dropdown to select property characteristics
+# selected_property = st.selectbox("Выберите участок", property_characteristics)
+
+# # You can use the selected_property in your application as needed
+# st.write("Вы выбрали следующий участок:", selected_property)
+# distance_1000m_ddo = st.checkbox("Наличие ДДО в радиусе 1 км", key="distance_1000m_ddo")
+# distance_1000m_schools = st.checkbox("Наличие школы в радиусе 1 км", key="distance_1000m_schools")
+# distance_1000m_medical = st.checkbox("Наличие медучреждения в радиусе 1 км", key="distance_1000m_medical")
+# is_parking_exists = st.checkbox("Наличие парковки в радиусе 1 км", key="is_parking_exists")
+# distance_park_1000m = st.checkbox("Наличие парка в радиусе 1 км", key="distance_park_1000m")
+# distance_bikeroad_1000m = st.checkbox("Наличие велодорожки в радиусе 1 км", key="distance_bikeroad_1000m")
+# deficit_ddo = st.number_input("Дефицит ДДО", min_value=0, key="deficit_ddo")
+# deficit_schools = st.number_input("Дефицит школ", min_value=0, key="deficit_schools")
+# amount_dosug_1000m = st.number_input("Количество объектов досуга в радиусе 1000м", min_value=0, key="amount_dosug_1000m")
+# amount_of_cameras_1000m = st.number_input("Количество камер видеонаблюдения в радиусе 1000м", min_value=0, key="amount_of_cameras_1000m")
+# amount_of_bins_1000m = st.number_input("Количество коммерческих организации в радиусе 1000м", min_value=0, key="amount_of_bins_1000m")
+# amount_of_poi_1000m = st.number_input("Количество объектов благоустройства в радиусе 1000м", min_value=0, key="amount_of_poi_1000m")
+# amount_of_business19_1000m = st.number_input("Количество предприятий общественного питания в радиусе 1000м", min_value=0, key="amount_of_business19_1000m")
+# index_of_nearest_sensor_pm = st.number_input("Показатель датчика качества воздуха после полудня", min_value=0, key="index_of_nearest_sensor_pm")
+# index_of_nearest_sensor_am = st.number_input("Показатель датчика качества воздуха до полудня", min_value=0, key="index_of_nearest_sensor_am")
+
+# # Button to make predictions
+# if st.button("Предсказать стоимость", key="prediction_button"):
+#     # Data preprocessing
+#     features = np.array([distance_1000m_ddo, distance_1000m_schools,
+#                      distance_1000m_medical, deficit_ddo, deficit_schools, amount_dosug_1000m, is_parking_exists,
+#                      distance_park_1000m, amount_of_cameras_1000m, distance_bikeroad_1000m, amount_of_bins_1000m,
+#                      amount_of_poi_1000m, amount_of_business19_1000m, index_of_nearest_sensor_pm,
+#                      index_of_nearest_sensor_am]).reshape(1, -1)
+    
+#     # Predict property price
+#     predicted_price = model.predict(features)[0]
+    
+#     # Apply the minimum and maximum limits
+#     predicted_price = min(max(predicted_price, 80000), 350000)
+    
+#     # Display the prediction result
+#     st.header("Результат прогноза")
+#     st.markdown(
+#         """
+#         <style>
+#         [data-testid="stMetricValue"] {
+#             font-size: 40px;
+#             color: green;
+#         }
+#         </style>
+#         """,
+#         unsafe_allow_html=True,
+#     )
+#     st.metric(label='Стоимость земли (м²)', value=f"₸ {predicted_price:.0f}", delta=None)
+
+st.markdown("""
+##### Прогноз стоимости земли после программы реновации
+            """)
+
 # Input fields for property characteristics
 st.header("Характеристики земельного участка")
-distance_1000m_ddo = st.checkbox("Наличие ДДО в радиусе 1 км")
-distance_1000m_schools = st.checkbox("Наличие школы в радиусе 1 км")
-distance_1000m_medical = st.checkbox("Наличие медучреждения в радиусе 1 км")
-is_parking_exists = st.checkbox("Наличие парковки в радиусе 1 км")
-distance_park_1000m = st.checkbox("Наличие парка в радиусе 1 км")
-distance_bikeroad_1000m = st.checkbox("Наличие велодорожки в радиусе 1 км")
-deficit_ddo = st.number_input("Дефицит ДДО", min_value=0)
-deficit_schools = st.number_input("Дефицит школ", min_value=0)
-amount_dosug_1000m = st.number_input("Количество объектов досуга в радиусе 1000м", min_value=0)
-amount_of_cameras_1000m = st.number_input("Количество камер видеонаблюдения в радиусе 1000м", min_value=0)
-amount_of_bins_1000m = st.number_input("Количество коммерческих организации в радиусе 1000м", min_value=0)
-amount_of_poi_1000m = st.number_input("Количество объектов благоустройства в радиусе 1000м", min_value=0)
-amount_of_business19_1000m = st.number_input("Количество предприятий общественного питания в радиусе 1000м", min_value=0)
-index_of_nearest_sensor_pm = st.number_input("Показатель датчика качества воздуха после полудня", min_value=0)
-index_of_nearest_sensor_am = st.number_input("Показатель датчика качества воздуха до полудня", min_value=0)
+new_distance_1000m_ddo = st.checkbox("Наличие ДДО в радиусе 1 км", key="new_distance_1000m_ddo")
+new_distance_1000m_schools = st.checkbox("Наличие школы в радиусе 1 км", key="new_distance_1000m_schools")
+new_distance_1000m_medical = st.checkbox("Наличие медучреждения в радиусе 1 км", key="new_distance_1000m_medical")
+new_is_parking_exists = st.checkbox("Наличие парковки в радиусе 1 км", key="new_is_parking_exists")
+new_distance_park_1000m = st.checkbox("Наличие парка в радиусе 1 км", key="new_distance_park_1000m")
+new_distance_bikeroad_1000m = st.checkbox("Наличие велодорожки в радиусе 1 км", key="new_distance_bikeroad_1000m")
+new_deficit_ddo = st.number_input("Дефицит ДДО", min_value=0, key="new_deficit_ddo")
+new_deficit_schools = st.number_input("Дефицит школ", min_value=0, key="new_deficit_schools")
+new_amount_dosug_1000m = st.number_input("Количество объектов досуга в радиусе 1000м", min_value=0, key="new_amount_dosug_1000m")
+new_amount_of_cameras_1000m = st.number_input("Количество камер видеонаблюдения в радиусе 1000м", min_value=0, key="new_amount_of_cameras_1000m")
+new_amount_of_bins_1000m = st.number_input("Количество коммерческих организации в радиусе 1000м", min_value=0, key="new_amount_of_bins_1000m")
+new_amount_of_poi_1000m = st.number_input("Количество объектов благоустройства в радиусе 1000м", min_value=0, key="new_amount_of_poi_1000m")
+new_amount_of_business19_1000m = st.number_input("Количество предприятий общественного питания в радиусе 1000м", min_value=0, key="new_amount_of_business19_1000m")
+new_index_of_nearest_sensor_pm = st.number_input("Показатель датчика качества воздуха после полудня", min_value=0, key="new_index_of_nearest_sensor_pm")
+new_index_of_nearest_sensor_am = st.number_input("Показатель датчика качества воздуха до полудня", min_value=0, key="new_index_of_nearest_sensor_am")
+
+
+
+
+
 
 # Button to make predictions
-if st.button("Предсказать стоимость", key="prediction_button"):
+if st.button("Предсказать стоимость", key="new_prediction_button"):
     # Data preprocessing
-    features = np.array([distance_1000m_ddo, distance_1000m_schools,
-                         distance_1000m_medical, deficit_ddo, deficit_schools, amount_dosug_1000m, is_parking_exists,
-                         distance_park_1000m, amount_of_cameras_1000m, distance_bikeroad_1000m, amount_of_bins_1000m,
-                         amount_of_poi_1000m, amount_of_business19_1000m, index_of_nearest_sensor_pm,
-                         index_of_nearest_sensor_am]).reshape(1, -1)
+    new_features = np.array([new_distance_1000m_ddo, new_distance_1000m_schools,
+                         new_distance_1000m_medical, new_deficit_ddo, new_deficit_schools, new_amount_dosug_1000m, new_is_parking_exists,
+                         new_distance_park_1000m, new_amount_of_cameras_1000m, new_distance_bikeroad_1000m, new_amount_of_bins_1000m,
+                         new_amount_of_poi_1000m, new_amount_of_business19_1000m, new_index_of_nearest_sensor_pm,
+                         new_index_of_nearest_sensor_am]).reshape(1, -1)
     
     # Predict property price
-    predicted_price = model.predict(features)[0]
+    new_predicted_price = model.predict(new_features)[0]
     
     # Apply the minimum and maximum limits
-    predicted_price = min(max(predicted_price, 80000), 350000)
+    new_predicted_price = min(max(new_predicted_price, 80000), 350000)
     
     # Display the prediction result
     st.header("Результат прогноза")
@@ -98,7 +162,7 @@ if st.button("Предсказать стоимость", key="prediction_button
         """,
         unsafe_allow_html=True,
     )
-    st.metric(label='Стоимость земли (м²)', value=f"₸ {predicted_price:.0f}", delta=None)
+    st.metric(label='Стоимость земли (м²)', value=f"₸ {new_predicted_price:.0f}", delta=None)
 
 # Add a section for the Leaflet map
 st.header("Карта земельных участков")
